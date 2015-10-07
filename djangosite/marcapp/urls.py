54434182193255b2c6021e11bc4d446c5d7bc7eb
vonmises <vonmises@users.noreply.github.com>
2015-10-07 22:50:15 +0300

    added urls for the forms

--- djangosite/marcapp/urls.py
+++ djangosite/marcapp/urls.py
@@ -3,5 +3,9 @@ from django.conf.urls import url
 urlpatterns = [ url(r'^user/(?P<username>[-\w]+)/$',
                     'marcapp.views.bookmark_user',
                     name='marcapp_bookmark_user'),
+                url(r'^create/$', 'marcapp.views.bookmark_create',
+                    name='marcapp_bookmark_create'),
+                url(r'^edit/(?P<pk>\d+)/$', 'marcapp.views.bookmark_edit',
+                    name='marcapp_bookmark_edit'),
                 url(r'^$', 'marcapp.views.bookmark_list',
                     name='marcapp_bookmark_list'),]
